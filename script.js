30a2a1bf94b9092b6c46d6654c6bf3a79c29d271
jfdsajfdsa <64916862+jfdsajfdsa@users.noreply.github.com>
2020-06-04 00:04:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById("test").innerHTML = "こんにちは！";
-console.log("Hello World");
+  document.getElementById("test").innerHTML = "HELLO WORLD";
+  
+
+//時刻
+  function showClock1(){
+    var nowTime = new Date(); 
+    var nowYear = nowTime.getFullYear();
+    var nowMon  = nowTime.getMonth() + 1;
+    var nowDate = nowTime.getDate();
+    var nowHour = nowTime.getHours();
+    var nowMin  = nowTime.getMinutes();
+    var nowSec  = nowTime.getSeconds();
+    var msg = "現在" + nowYear +"/" + nowMon +"/"+ nowDate + " - " + nowHour +":" + nowMin + ":" + nowSec + " です。 ";
+    document.getElementById("RealtimeClockArea").innerHTML = msg;
+ }
+  setInterval('showClock1()',1000);
 
